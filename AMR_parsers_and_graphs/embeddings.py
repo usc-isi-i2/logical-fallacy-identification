0633cc2217e5f47b6b04c329f3dd450a30373090
div22loaner <div22loaner@D22ML-LOANER3.local>
2022-09-07 15:11:07 -0700

    Add the results for the GCN embedding the nodal features

--- AMR_parsers_and_graphs/embeddings.py
+++ AMR_parsers_and_graphs/embeddings.py
@@ -14,24 +14,22 @@ model = BertModel.from_pretrained(model_name)
 
 
 def get_bert_embeddings(word: str) -> th.Tensor:
-    input_text = word
-    input_ids = tokenizer.encode(input_text, add_special_tokens=False, return_tensors = 'pt')
+    try:
+        input_text = word
+        input_ids = tokenizer.encode(
+            input_text, add_special_tokens=False, return_tensors='pt')
 
-    outputs = model(input_ids)
-    return outputs['last_hidden_state'].squeeze()
-    
+        outputs = model(input_ids)
+        outputs = outputs['last_hidden_state']
+    except Exception as e:
+        embed()
+    return th.mean(outputs, dim=(0, 1))
 
 
 if __name__ == "__main__":
-    amr_graphs_with_sentences = joblib.load(PATH_TO_MASKED_SENTENCES_AMRS)
-    sample_graph = amr_graphs_with_sentences[0][1].graph_nx
-    
-    
-
-    
-        
-
-
-
-        
-
+    word = "degree"
+    embedding = get_bert_embeddings(word)
+    print(embedding.shape)
+    embed()
+    # amr_graphs_with_sentences = joblib.load(PATH_TO_MASKED_SENTENCES_AMRS)
+    # sample_graph = amr_graphs_with_sentences[0][1].graph_nx
--- AMR_parsers_and_graphs/eval.py
+++ AMR_parsers_and_graphs/eval.py
@@ -1,3 +1,6 @@
+from sklearn.metrics import classification_report
+from collections import defaultdict
+from torch_geometric.data import InMemoryDataset
 import numpy as np
 import pandas as pd
 from IPython import embed
@@ -41,6 +44,32 @@ def compute_random_baseline(fallacy_types_counts: pd.Series, fallacy_type: str, 
             size=top_n,
             p=list(type_prob_dict.values())
         )
-        predictions = (np.array([fallacy_type]) == np.array(predictions)).astype(int)
+        predictions = (np.array([fallacy_type]) ==
+                       np.array(predictions)).astype(int)
         results.append(predictions)
     return mean_average_precision(np.array(results))
+
+
+def get_random_predictions_for_pyg_metrics(dataset: InMemoryDataset, all_test_true_labels, size: int):
+    label2index = dataset.label2index
+
+    classes_counts = defaultdict(int)
+    for data in dataset:
+        classes_counts[data.y.item()] += 1
+
+    classes_probs = {
+        key: value / dataset.len()
+        for key, value in classes_counts.items()
+    }
+
+    all_test_true_labels_extended = all_test_true_labels * size
+    all_test_predictions_random = np.random.choice(
+        a=list(label2index.keys()),
+        size=len(all_test_true_labels_extended),
+        p=list(classes_probs.values())
+    )
+
+    return classification_report(
+        y_pred=all_test_predictions_random,
+        y_true=all_test_true_labels_extended
+    )
