c2634d93abb0e7a2ec72d2933a7449a93d4c2e15
Zhivar Zadeh <souratih@ckg01.isi.edu>
2022-09-07 22:56:24 -0700

    Add the files associated with the results so far

--- AMR_parsers_and_graphs/graph_kernels.py
+++ AMR_parsers_and_graphs/graph_kernels.py
@@ -1,5 +1,6 @@
 from grakel import Graph
 from grakel import GraphKernel
+from sklearn.metrics import pairwise_distances
 import networkx as nx
 from IPython import embed
 import joblib
@@ -10,6 +11,12 @@ from embeddings import get_bert_embeddings
 import re
 
 from consts import PATH_TO_MASKED_SENTENCES_AMRS, PATH_TO_MOST_SIMILAR_GRAPHS
+from sentence_transformers import SentenceTransformer
+
+import warnings
+warnings.filterwarnings("ignore")
+
+model = None
 
 def get_label_mappings(amr_graphs_with_sentences):
     label2index = {}
@@ -77,11 +84,50 @@ def from_networkx(g: nx.Graph or nx.Digraph, edge_type2index) -> Graph:
     return Graph(edges, edge_labels=edge_labels)
 
 
+def get_graph_similarities_by_sentence_transformers(amr_graphs_with_sentences):
+    global model
+    if model is None:
+        model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+
+    embeddings = []
+    for obj in tqdm(amr_graphs_with_sentences, leave = False):
+        sentence = [obj[1].sentence]
+        embedding = model.encode(sentence)
+        embeddings.append(embedding.squeeze())
+    
+    embeddings = np.array(embeddings)
+    dist_out = 1-pairwise_distances(embeddings, metric="cosine")
+
+    similarity_matrix_df = pd.DataFrame(
+        dist_out,
+        index=np.arange(len(amr_graphs_with_sentences)),
+        columns=np.arange(len(amr_graphs_with_sentences))
+    )
+    
+    results = pd.DataFrame()
+
+    for index in similarity_matrix_df.index:
+        most_similar_graphs = similarity_matrix_df.loc[index].nlargest(10).index.tolist()
+        most_similar_graphs_scores = similarity_matrix_df.loc[index].nlargest(10).values
+        for similar_index, similarity_score in zip(most_similar_graphs, most_similar_graphs_scores):
+            if similar_index != index:
+                results = results.append({
+                    'sent_a': amr_graphs_with_sentences[index][1].sentence,
+                    'sent_b': amr_graphs_with_sentences[similar_index][1].sentence,
+                    'type_a': amr_graphs_with_sentences[index][2],
+                    'type_b': amr_graphs_with_sentences[similar_index][2],
+                    'similarity': similarity_score
+                }, ignore_index=True)
+    
+    
+    results.to_csv(f"{PATH_TO_MOST_SIMILAR_GRAPHS}{'transformers'}.csv", index = False)
 
 
-if __name__ == "__main__":
+
+
+
+def compute_graph_similarity_using_graph_kernel(kernel_name):
     amr_graphs_with_sentences = joblib.load(PATH_TO_MASKED_SENTENCES_AMRS)
-    label2index = get_label_mappings(amr_graphs_with_sentences=amr_graphs_with_sentences)
     edge_type2index = get_edge_mappings(amr_graphs_with_sentences=amr_graphs_with_sentences)
 
     grakel_graphs = []
@@ -93,8 +139,7 @@ if __name__ == "__main__":
                 edge_type2index=edge_type2index
             )
         )
-    
-    kernel_name = None
+
     kernel = GraphKernel(kernel=kernel_name, normalize = True)
 
 
@@ -129,14 +174,13 @@ if __name__ == "__main__":
             })], axis = 0, ignore_index = True)
 
     results.to_csv(f"{PATH_TO_MOST_SIMILAR_GRAPHS}{kernel_name}.csv", index = False)
-    
 
-        
-        
 
-        
-        
-        
 
-    
-        
+
+
+if __name__ == "__main__":
+    amr_graphs_with_sentences = joblib.load(PATH_TO_MASKED_SENTENCES_AMRS)
+    get_graph_similarities_by_sentence_transformers(
+        amr_graphs_with_sentences=amr_graphs_with_sentences
+    )
