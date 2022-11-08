3c67760fb309590241b82ba4acad810f52ce2da2
himanshurawlani <himanshurawlani@hotmail.com>
2022-11-08 23:52:06 +0100

    Updated NLP data augmentation script

--- data_augmentation/nlp_aug_data.py
+++ data_augmentation/nlp_aug_data.py
@@ -3,8 +3,10 @@ import numpy as np
 from tqdm import tqdm
 import nlpaug.augmenter.word as naw
 import torch
+import argparse
 
-def augment(samples, class_, df, aug, times):
+
+def augment(samples, class_, df, aug, times, text_column="text", label_column="label"):
     counter = 0
     np.random.shuffle(samples)
     repetitions = (times // len(samples)) + 1
@@ -12,10 +14,14 @@ def augment(samples, class_, df, aug, times):
     for s in tqdm(np.repeat(samples, repetitions)):
         if counter == times:
             break
-        df = df.append({"text": aug.augment(s), "label":class_}, ignore_index=True)
+        df = pd.concat(
+            [df, pd.DataFrame.from_dict({text_column: aug.augment(s), label_column: class_})],
+            ignore_index=True,
+        )
         counter += 1
-  
+
     return df
+
 
 def main(train_csv, output_csv, text_column="text", label_column="label"):
 
@@ -27,7 +33,7 @@ def main(train_csv, output_csv, text_column="text", label_column="label"):
     max_val = max(value_counts.values())
 
     # Decides how much to augment for every class. Change the `max_val` to change this
-    differences = {key: max_val-value for key,value in value_counts.items()}
+    differences = {key: max_val - value for key, value in value_counts.items()}
 
     # Check if GPU is available
     if torch.cuda.is_available():
@@ -35,13 +41,27 @@ def main(train_csv, output_csv, text_column="text", label_column="label"):
     else:
         device = "cpu"
 
-    # Initialize augmenter
-    aug = naw.ContextualWordEmbsAug(model_path='roberta-base', top_k=20, aug_p=0.2, action="substitute", device=device)
+    # Initialize augmenter with specific parameters
+    aug = naw.ContextualWordEmbsAug(
+        model_path="roberta-base",
+        top_k=20,
+        aug_p=0.2,
+        action="substitute",
+        device=device,
+    )
 
     # Augment classes
     for key, value in differences.items():
         print(f"Augmenting class: {key}")
-        df_train = augment(df_train[df_train[label_column]==key][text_column].to_numpy(), key, df_train, aug, value)
+        df_train = augment(
+            df_train[df_train[label_column] == key][text_column].to_numpy(),
+            key,
+            df_train,
+            aug,
+            value,
+            text_column,
+            label_column,
+        )
 
     # Augmented dataset count
     print(f"After augmentation: {df_train[label_column].value_counts().to_dict()}")
@@ -49,12 +69,12 @@ def main(train_csv, output_csv, text_column="text", label_column="label"):
     df_train.to_csv(output_csv, index=False)
 
 
-if __name__ =="__main__":
-    input_csv = "ptc_slc_without_none_with_context/coarse/train.csv"
-    output_csv = "ptc_slc_without_none_with_context_nlpaug/coarse/train.csv"
-    main(input_csv, output_csv)
-
-    input_csv = "ptc_slc_without_none_with_context/fine/train.csv"
-    output_csv = "ptc_slc_without_none_with_context_nlpaug/fine/train.csv"
-    main(input_csv, output_csv)
-    
+if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_csv")
+    parser.add_argument("--output_csv")
+    parser.add_argument("--text_column", default="text")
+    parser.add_argument("--label_column", default="label")
+    args = parser.parse_args()
+    main(args.input_csv, args.output_csv, args.text_column, args.label_column)
