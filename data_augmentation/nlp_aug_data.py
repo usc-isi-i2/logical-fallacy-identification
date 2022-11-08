87102c10574165e8ca6357b39707c7a8d73cc933
himanshurawlani <himanshurawlani@hotmail.com>
2022-11-09 00:37:08 +0100

    Added max_upsampling parameter

--- data_augmentation/nlp_aug_data.py
+++ data_augmentation/nlp_aug_data.py
@@ -15,7 +15,12 @@ def augment(samples, class_, df, aug, times, text_column="text", label_column="l
         if counter == times:
             break
         df = pd.concat(
-            [df, pd.DataFrame.from_dict({text_column: aug.augment(s), label_column: class_})],
+            [
+                df,
+                pd.DataFrame.from_dict(
+                    {text_column: aug.augment(s), label_column: class_}
+                ),
+            ],
             ignore_index=True,
         )
         counter += 1
@@ -23,17 +28,19 @@ def augment(samples, class_, df, aug, times, text_column="text", label_column="l
     return df
 
 
-def main(train_csv, output_csv, text_column="text", label_column="label"):
+def main(train_csv, output_csv, max_upsampling=None, text_column="text", label_column="label"):
 
     df_train = pd.read_csv(train_csv, usecols=[text_column, label_column])
 
     value_counts = df_train[label_column].value_counts().to_dict()
     print(f"Before augmentation: {value_counts}")
 
-    max_val = max(value_counts.values())
-
-    # Decides how much to augment for every class. Change the `max_val` to change this
-    differences = {key: max_val - value for key, value in value_counts.items()}
+    if max_upsampling:
+        differences = {key: max(0, max_upsampling-value) for key, value in value_counts.items()}
+    else:
+        max_val = max(value_counts.values())
+        # Decides how much to augment for every class. Change the `max_val` to change this
+        differences = {key: max_val - value for key, value in value_counts.items()}
 
     # Check if GPU is available
     if torch.cuda.is_available():
@@ -44,7 +51,7 @@ def main(train_csv, output_csv, text_column="text", label_column="label"):
     # Initialize augmenter with specific parameters
     aug = naw.ContextualWordEmbsAug(
         model_path="roberta-base",
-        top_k=10,
+        top_k=5,
         aug_p=0.2,
         action="substitute",
         device=device,
@@ -74,7 +81,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_csv")
     parser.add_argument("--output_csv")
+    parser.add_argument("--max_upsampling", default=None, type=int)
     parser.add_argument("--text_column", default="text")
     parser.add_argument("--label_column", default="label")
     args = parser.parse_args()
-    main(args.input_csv, args.output_csv, args.text_column, args.label_column)
+    main(
+        args.input_csv,
+        args.output_csv,
+        args.max_upsampling,
+        args.text_column,
+        args.label_column,
+    )
